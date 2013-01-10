5b3b6bd4d926887e6424505385ed7dfec8c4038c
Edward Banner <edward.banner@gmail.com>
2013-01-10 01:16:31 -0500

    changed athena challenge back to the way it should have been

--- misc/athena_challenge.cpp
+++ misc/athena_challenge.cpp
@@ -4,8 +4,6 @@
 #include <algorithm>
 #include <cstdlib>
 
-#define npaths(x,y) (legal_loc(board, loc(x,y), rows, columns)? \ num_paths_recursive(board, loc(x,y), num_moves+1, rows, columns) : 0)
-
 class loc {
   public:
     // constructor
@@ -15,12 +13,7 @@ class loc {
     int c;
 };
 
-bool legal_loc(std::vector<std::vector<bool> > board, const loc &pos, const int rows, const int columns)
-{
-  return ((0 <= pos.r && pos.r <= rows-1) && (0 <= pos.c && pos.c <= columns-1) && ! board[pos.r][pos.c]);
-}
-
-int num_paths_recursive(std::vector<std::vector<bool> > board, const loc &pos, int num_moves, const int rows, const int columns)
+int num_paths_recursive(std::vector<std::vector<bool> > board, loc pos, int num_moves, int rows, int columns)
 {
   if (pos.r == rows-1 && pos.c == 0) { // base case: you're in the bottom left corner
     if (num_moves == rows*columns) {
@@ -28,28 +21,23 @@ int num_paths_recursive(std::vector<std::vector<bool> > board, const loc &pos, i
     } else {
       return 0; // didn't visit every square
     }
-  } else { 
-    //mark this position as being visited.
+  } else if ((0 <= pos.r && pos.r <= rows-1) && (0 <= pos.c && pos.c <= columns-1) && ! board[pos.r][pos.c]) { 
+    /* Legal r & c coordinates and this location has not yet been visited.
+     *
+     * Mark this position as being visited. */
     board[pos.r][pos.c] = true;
 
-    int sum = 0;
-    if ((0 <= pos.r-1 && pos.r-1 <= rows-1) && (0 <= pos.c && pos.c <= columns-1) && ! board[pos.r-1][pos.c])
-      sum += num_paths_recursive(board, loc(pos.r-1,pos.c), num_moves+1, rows, columns);
+    return num_paths_recursive(board, loc(pos.r-1,pos.c), num_moves+1, rows, columns) +
+           num_paths_recursive(board, loc(pos.r,pos.c+1), num_moves+1, rows, columns) +
+           num_paths_recursive(board, loc(pos.r+1,pos.c), num_moves+1, rows, columns) +
+           num_paths_recursive(board, loc(pos.r,pos.c-1), num_moves+1, rows, columns);
 
-    if ((0 <= pos.r && pos.r <= rows-1) && (0 <= pos.c+1 && pos.c+1 <= columns-1) && ! board[pos.r][pos.c+1])
-      sum += num_paths_recursive(board, loc(pos.r,pos.c+1), num_moves+1, rows, columns);
-
-    if ((0 <= pos.r+1 && pos.r+1 <= rows-1) && (0 <= pos.c && pos.c <= columns-1) && ! board[pos.r+1][pos.c])
-      sum += num_paths_recursive(board, loc(pos.r+1,pos.c), num_moves+1, rows, columns);
-
-    if ((0 <= pos.r && pos.r <= rows-1) && (0 <= pos.c-1 && pos.c-1 <= columns-1) && ! board[pos.r][pos.c-1])
-      sum += num_paths_recursive(board, loc(pos.r,pos.c-1), num_moves+1, rows, columns);
-
-    return sum;
+  } else { // this location is not legal
+    return 0;
   }
 }
 
-int num_paths(const int rows, const int columns)
+int num_paths(int rows, int columns)
 {
   std::vector<std::vector<bool> > board(rows);
   for (int r = 0; r < rows; r++)
