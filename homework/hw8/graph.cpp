cd0d2c9a9063f486ccb43fe5a95b327ab044eb9e
Edward Banner <edward.banner@gmail.com>
2013-01-15 01:15:57 -0500

    started working on implementing `graph'

---
+++ homework/hw8/graph.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+
+bool Graph::addCity(const std::string& city_name) {
+  City *city = getCityWithName(city_name);
+
+
+bool removeCity(const std::string& city_name);
+bool addLink(const std::string& city_name1, const std::string& city_name2);
+bool removeLink(const std::string& city_name1, const std::string& city_name2);
+bool placePursuer(const std::string& person_name, const std::string& city_name);
+bool placeEvader(const std::string& person_name, const std::string& city_name);
+
+
+City* City::getCityWithName(const std::string& name) const {
+
